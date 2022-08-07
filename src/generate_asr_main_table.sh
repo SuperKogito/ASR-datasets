45472a0ec1f03035291d38657aa29f564dc9a309
SuperKogito <superkogito@gmail.com>
2022-08-07 22:21:37 +0200

    restructure

--- src/generate_asr_main_table.sh
+++ src/generate_asr_main_table.sh
@@ -1,18 +1,18 @@
 #!/bin/zsh
 echo "Language,# Datasets, # Hours,# Corpora,# Words, Last update"
-for f in languages/csv/asr-*.csv;
+for f in languages/csv/speech/asr-*.csv;
 do
 	lang=$(echo $f | grep -o -P '(?<=s-).*(?=.c)');
 	clang=$(echo ${(C)lang})
 	link=$(echo "\`$clang <./languages/${lang}.html>\`_");
 	ndatasets=$(tail -n+2 $f | wc -l);
 	nhours=$(cat $f | awk -F "," '{s+=$4} END {print s}');
-	lu=$(git log -1 --pretty="format:%ci" languages/english.rst | awk -F " " '{print $1}') #$(date '+%Y-%m-%d');
+	lu=$(git log -1 --pretty="format:%ci" languages/${lang}.rst | awk -F " " '{print $1}') #$(date '+%Y-%m-%d');
 	ncorpora=0
 	nwords=0
-	if [ -f "languages/csv/corpora-$lang.csv" ]; then
-		ncorpora=$(tail -n+2 "languages/csv/corpora-$lang.csv" | wc -l);
-		nwords=$(cat "languages/csv/corpora-$lang.csv" | awk -F "," '{s+=$3} END {print s}')
+	if [ -f "languages/csv/text/corpora-$lang.csv" ]; then
+		ncorpora=$(tail -n+2 "languages/csv/text/corpora-$lang.csv" | wc -l);
+		nwords=$(cat "languages/csv/text/corpora-$lang.csv" | awk -F "," '{s+=$3} END {print s}')
 	fi
 	echo $link,$ndatasets,$nhours,$ncorpora,$nwords,$lu;
 done;
